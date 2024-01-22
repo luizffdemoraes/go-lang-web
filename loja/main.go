7888b8b42fda3805446f47a1c415d9a250c93694
Luiz Felipe <lffm1994@gmail.com>
2024-01-22 20:29:04 -0300

    Conectando com o banco

--- loja/main.go
+++ loja/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB{
+	conexao := "user=postgres dbname=alura_loja password= host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +27,8 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("loja/templates/*.html"))
 
 func main() {
+	db := conectaComBancoDeDados();
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
