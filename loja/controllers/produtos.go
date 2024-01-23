521854b0c7c383a9299cd86a8fdbb1bae919f723
Luiz Felipe <lffm1994@gmail.com>
2024-01-23 00:14:44 -0300

    Página de novos produtos

--- loja/controllers/produtos.go
+++ loja/controllers/produtos.go
@@ -12,3 +12,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	produtos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
