4ce0c0a70e40025ea8d39da787cd4e9691515cb0
Luiz Felipe <lffm1994@gmail.com>
2024-01-23 01:25:10 -0300

    Salvando produto no banco

--- loja/controllers/produtos.go
+++ loja/controllers/produtos.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"log"
 	"loja/models"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -14,5 +16,27 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func New(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "New", nil)
+    temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão de quantidade", err)
+		}
+
+		models.CriaNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+	}
+	http.Redirect(w, r, "/", http.StatusMovedPermanently)
 }
